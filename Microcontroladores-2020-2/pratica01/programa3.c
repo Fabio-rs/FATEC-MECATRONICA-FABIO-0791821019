2c43610357deada0504ca5b1787d94532076fa20
Fabio-rs <69904584+Fabio-rs@users.noreply.github.com>
2020-08-19 10:27:38 -0300

    Create programa3.c

---
+++ Microcontroladores-2020-2/pratica01/programa3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+ int n1, n2, s;
+ printf("informe um numero:");
+ scanf("%i", &n1);
+ printf("informe um numero:");
+ scanf("%i", &n2);
+ s = n1 + n2;
+ printf("Resultado: %i\n", s);
+  return 0;
+}
