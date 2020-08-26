ac39340cedb750ac1b8eff03b7f5f0857d4c0ee8
Fabio-rs <69904584+Fabio-rs@users.noreply.github.com>
2020-08-26 08:36:36 -0300

    Update programa1.c

--- Microcontroladores-2020-2/Pratica02/programa1.c
+++ Microcontroladores-2020-2/Pratica02/programa1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main(void) {
+  float A,B,X,y;
+  printf("informe o valor de A,B e X:");
+  scanf("%f%f%f" , &A, &B, &X);
+  y = (A*X) + B;
+  printf("resultado da equacao: %.3f\n" , y);
+
+  return 0;
+}
