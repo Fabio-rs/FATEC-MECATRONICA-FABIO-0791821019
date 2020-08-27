bc337a67458b857efa6fda371a7e489e5ec70e4d
Fabio-rs <69904584+Fabio-rs@users.noreply.github.com>
2020-08-27 10:35:15 -0300

    Create programa03.py

---
+++ LTP2 2020-2/PRATICA02/programa03.py
@@ -0,0 +1,5 @@
+#usuario fala dois numeros e nós calculamos a média deles
+numero1 = int(input('numero1:'))
+numero2 = int(input('numero2:'))
+media = (numero1 + numero2) /2
+print('resultado da media:', media)
